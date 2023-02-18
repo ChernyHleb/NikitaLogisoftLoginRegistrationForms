bae35d6c25b4262d2699f1ffeca1a4040db8876c
ChernyHleb <popovarlol@gmail.com>
2023-02-18 05:32:49 +0300

    registration form implemented

--- NikitaLogisoftLoginRegistrationForms/RegistrationWindow.xaml.cs
+++ NikitaLogisoftLoginRegistrationForms/RegistrationWindow.xaml.cs
@@ -10,26 +10,29 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace NikitaLogisoftLoginRegistrationForms
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for RegistrationWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class RegistrationWindow : Window
     {
-        public MainWindow()
+        public RegistrationWindow()
         {
             InitializeComponent();
-            RegistrationWindow registrationWindow = new RegistrationWindow();
-            registrationWindow.Show();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            var desktopWorkingArea = System.Windows.SystemParameters.WorkArea;
+            this.Left = desktopWorkingArea.Right - this.Width * 1.25;
         }
 
         private void Window_MouseDown(Object sender, MouseButtonEventArgs e)
         {
-            if(e.LeftButton== MouseButtonState.Pressed)
+            if (e.LeftButton == MouseButtonState.Pressed)
             {
                 DragMove();
             }
