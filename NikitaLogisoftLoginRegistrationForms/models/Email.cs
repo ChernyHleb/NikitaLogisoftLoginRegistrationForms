371bf602c8f90a4e113ffaac472149a4fe8f0898
ChernyHleb <popovarlol@gmail.com>
2023-03-01 15:25:56 +0300

    deleted unused references

---
+++ NikitaLogisoftLoginRegistrationForms/models/Email.cs
@@ -0,0 +1,7 @@
+﻿namespace NikitaLogisoftLoginRegistrationForms.models
+{
+    internal class Email
+    {
+        public string email { get; set; }
+    }
+}
--- NikitaLogisoftLoginRegistrationForms/models/User.cs
+++ NikitaLogisoftLoginRegistrationForms/models/User.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace NikitaLogisoftLoginRegistrationForms.models
+﻿namespace NikitaLogisoftLoginRegistrationForms.models
 {
     internal class User
     {
